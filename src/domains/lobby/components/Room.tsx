805f7de011a81040e73097619bbf6531ee7954a4
Natthapong Intharak <nattha.int@gmail.com>
2020-04-15 17:35:13 +0700

    Add room

--- src/domains/lobby/components/Room.tsx
+++ src/domains/lobby/components/Room.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
-class Room extends React.Component {
+interface RoomProps {
+  creator: string,
+  gameName: string,
+  noOfPlayers: number,
+  maxPlayers: number
+}
+
+class Room extends React.Component<RoomProps> {
+
+  public static defaultProps = {
+    creator: 'Creator',
+    gameName: 'GameName',
+    noOfPlayers: 1,
+    maxPlayers: 10
+  }
+
   render() {
+    const { creator, gameName, noOfPlayers, maxPlayers } = this.props
     return(
       <div>
-        <a>hello</a>
+        <label>{ creator }</label>
+        <label>{ gameName }</label>
+        <label>{ noOfPlayers }</label>
+        <label>{ maxPlayers }</label>
       </div>
     )
   }
--- src/domains/lobby/components/__tests__/Room.tsx
+++ src/domains/lobby/components/__tests__/Room.tsx
@@ -4,10 +4,18 @@ import Room from '../Room';
 
 describe('Room', () => {
 
-  it('should works', () => {
-    const { getByText } = render(<Room />)
-    const linkElement = getByText(/hello/)
-    expect(true).toBe(true)
+  it('should contains correct props', () => {
+    const { getByText } = render(
+      <Room
+        creator={'creator'}
+        gameName={'gameName'}
+        maxPlayers={ 10 }
+        noOfPlayers={ 3 }/>)
+
+    expect(getByText(/creator/)).toBeInTheDocument()
+    expect(getByText(/gameName/)).toBeInTheDocument()
+    expect(getByText(/10/)).toBeInTheDocument()
+    expect(getByText(/3/)).toBeInTheDocument()
   })
   
 })
